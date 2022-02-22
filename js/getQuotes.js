2f6f0f8add99cdf3b5351bff380009acf99ed2dc
Courtney Ellis <courtneycaloca@gmail.com>
2022-02-21 21:09:48 -0700

    Creative Project 2 (JS)

--- js/getQuotes.js
+++ js/getQuotes.js
@@ -1,12 +1,24 @@
-const url = "https://inspiration.goprogram.ai";
-fetch(url)
-    .then(function (response) {
-        return response.json();
-    }).then(function (json) {
-        console.log(json);
-        console.log(json[0]);
-        const quote = json[0];
-        const author = json[1];
-        document.getElementById("quote").innerHTML = quote;
-        document.getElementById("author").innerHTML = author;
-    });
+// Previous Code
+
+
+function getQuote() {
+    let url = "https://cors-anywhere.herokuapp.com";
+    url += "/inspiration.goprogram.ai";
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            console.log('json: ' + json);
+            const quote = json.quote;
+            const author = json.author;
+            document.getElementById("quote").innerHTML = "\"" + quote + "\"";
+            document.getElementById("author").innerHTML = "-" + author;
+        })
+        .catch(function (reject) {
+            const quote = "When you exceed the 'quote of the day' API call limits, gotta have a backup quote."
+            const author = "The Ellis's";
+            document.getElementById("quote").innerHTML = "\"" + quote + "\"";
+            document.getElementById("author").innerHTML = "-" + author;
+        });
+};
